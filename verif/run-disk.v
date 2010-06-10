30edb0dc03d92ebc04b23a795df59f1be43a6562
brad <brad@7350552a-1005-0410-a727-b2bbdf77da2d>
2010-06-10 13:40:44 +0000

    testing xbus-disk.v

--- verif/run-disk.v
+++ verif/run-disk.v
@@ -1,12 +1,16 @@
 /*
  */
 
+`define debug
+`define debug_detail
+
 `include "../rtl/busint.v"
 `include "../rtl/xbus-ram.v"
 `include "../rtl/xbus-disk.v"
 `include "../rtl/xbus-tv.v"
 `include "../rtl/xbus-io.v"
 `include "../rtl/xbus-unibus.v"
+`include "../rtl/ide.v"
 
 `timescale 1ns / 1ns
 
@@ -23,18 +27,30 @@ module test_busint;
    reg 	       write;
    wire        load;
    wire        interrupt;
-   
-   busint bi(.mclk(clk),
-	     .reset(reset),
-	     .addr(addr),
-	     .busin(busin),
-	     .busout(busout),
-	     .spy(spy), 
-	     .req(req),
-	     .ack(ack),
-	     .write(write), 
-	     .load(load),
-	     .interrupt(interrupt));
+
+   wire [15:0] 	ide_data_bus;
+   wire 	ide_dior;
+   wire 	ide_diow;
+   wire [1:0] 	ide_cs;
+   wire [2:0] 	ide_da;
+
+   busint busint(.mclk(clk),
+		 .reset(reset),
+		 .addr(addr),
+		 .busin(busin),
+		 .busout(busout),
+		 .spy(spy), 
+		 .req(req),
+		 .ack(ack),
+		 .write(write), 
+		 .load(load),
+		 .interrupt(interrupt),
+		 .ide_data_bus(ide_data_bus),
+		 .ide_dior(ide_dior),
+		 .ide_diow(ide_diow),
+		 .ide_cs(ide_cs),
+		 .ide_da(ide_da)
+		 );
 
 
    task wait_for_bi_done;
@@ -46,35 +62,41 @@ module test_busint;
    endtask
 
    task bus_read;
-      input [21:0] addr;
+      input [21:0] a;
 
       begin
+	 @(posedge clk);
 	 write = 0;
  	 req = 1;
+	 addr = a;
 	 wait_for_bi_done;
+	 $display("read: addr %o in %x out %x, %t",
+		  addr, busin, busout, $time);
 	 req = 0;
-	 $display("read: addr %o in %x out %x", addr, busin, busout);
       end
    endtask
 
    task bus_write;
-      input [21:0] addr;
+      input [21:0] a;
       input [31:0] data;
 
       begin
+	 @(posedge clk);
 	 write = 1;
+	 addr = a;
 	 busin = data;
  	 req = 1;
+	 $display("write: addr %o in %x out %x, %t",
+		  addr, busin, busout, $time);
 	 wait_for_bi_done;
 	 req = 0;
-	 $display("write: addr %o in %x out %x", addr, busin, busout);
       end
    endtask
 
    initial
      begin
 	$timeformat(-9, 0, "ns", 7);
-	$dumpfile("test_busint.vcd");
+	$dumpfile("run-disk.vcd");
 	$dumpvars(0, test_busint);
      end
 
@@ -85,15 +107,30 @@ module test_busint;
 	req = 0;
 	write = 0;
 
+	#1 reset = 1;
+	#100 reset = 0;
+
+	bus_write(22'o22, 32'o1001);
+	#100 bus_write(22'o23, 32'o4000);
+
 	bus_read(22'o17377774);
-	bus_write(22'o17377775, 0);
-	$finish;
+	bus_read(22'o17377774);
+	bus_read(22'o17377774);
+	bus_write(22'o17377775, 32'o22);
+	bus_write(22'o17377777, 32'o0);
+
+	#500000 $finish;
      end
 
    always
      begin
 	#10 clk = 0;
 	#10 clk = 1;
+     end
+
+   always @(posedge clk)
+     begin
+	$pli_ide(ide_data_bus, ide_dior, ide_diow, ide_cs, ide_da);
      end
 
 endmodule // test
